3809e211d983e0181356d9157df055ce4112117e
Shakhmurad <muradb356@gmail.com>
2023-11-12 14:32:13 +0400

    add usestate in modal

--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -1,10 +1,30 @@
-
+import {useState} from 'react'
 
 function Modal() {
+    const [modal , setmodal] = useState(false)
+
   return (
+    <>
     <div className="container-modal">
-        <p>Opne window</p>
+        <p onClick={()=>setmodal(true)} >Opne window</p>
     </div>
+    <div className={`modal ${modal? `window` : `none`}`}>
+        <div className='modal-box'>
+            <p>WELCOME</p>
+            <div className='modal-text-center'>
+                <p>this is window</p>
+                <div className="page-row">
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                </div>
+            </div>
+                <div className="modal-text-end">
+                    <p onClick={()=>setmodal(false)} > close</p>
+                </div>
+        </div>
+    </div>
+    </>
   )
 }
 
